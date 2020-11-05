9da5c58fe6737470cd84d06a45cbcd45500a3bd3
Jossic <jossic.lapierre@gmail.com>
2020-11-05 14:46:56 +0100

    movie-app - 13 - What ares props

--- components/MovieList.js
+++ components/MovieList.js
@@ -2,39 +2,12 @@ import React from 'react'
 
 class MovieList extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            count: 0
-        }
-    }
-
-    increment() {
-        // const count = this.state.count
-        const { count } = this.state
-        // this.state.count++
-        this.setState({
-            count: count + 1
-        })
-    }
-
-    decrement = () => {
-        // const count = this.state.count
-        const { count } = this.state
-        // this.state.count--
-        this.setState({
-            count: count - 1
-        })
-    }
 
     render() {
         return (
             <>
                 <div>
-                    <button onClick={() => this.increment()} className='btn btn-primary'>+ num</button>
-                    <button onClick={this.decrement} className='btn btn-primary'>- num</button>
-
-                    <h1>{this.state.count}</h1>
+                    <h1>{this.props.count}</h1>
                 </div>
 
                 <div className="col-lg-4 col-md-6 mb-4">
--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,31 +1,17 @@
 import React, { useState } from 'react'
 
 
-const SideMenu = () => {
-    const [count, setCount] = useState(0)
-
-    const increment = () => {
-        const plus = count + 1
-        setCount(plus)
-    }
-
-    const decrement = () => {
-        const moins = count - 1
-        setCount(moins)
-    }
-
+const SideMenu = ({ count, appName, clickHandler }) => {
     return (
         <>
-            <h1 className="my-4">Baby-Hoshi</h1>
+            <h1 className="my-4">{appName}</h1>
             <div className="list-group">
                 <a href="#" className="list-group-item">Category 1</a>
                 <a href="#" className="list-group-item">Category 2</a>
                 <a href="#" className="list-group-item">Category 3</a>
             </div>
             <div>
-                <button onClick={() => increment()} className='btn btn-primary'>+ num</button>
-                <button onClick={decrement} className='btn btn-primary'>- num</button>
-
+                <button onClick={clickHandler} className='btn btn-dark'>Cliquer sur moi</button>
                 <h1>{count}</h1>
             </div>
         </>
