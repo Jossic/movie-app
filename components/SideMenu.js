b5c8f0bb5d1851c4e681c3b7fa3adeb00b1f96b4
Jossic <jossic.lapierre@gmail.com>
2020-11-05 14:28:14 +0100

    movie-app - 12 - State of functionnal component

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const SideMenu = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        const plus = count + 1
+        setCount(plus)
+    }
+
+    const decrement = () => {
+        const moins = count - 1
+        setCount(moins)
+    }
+
     return (
         <>
             <h1 className="my-4">Baby-Hoshi</h1>
@@ -8,6 +21,12 @@ const SideMenu = () => {
                 <a href="#" className="list-group-item">Category 1</a>
                 <a href="#" className="list-group-item">Category 2</a>
                 <a href="#" className="list-group-item">Category 3</a>
+            </div>
+            <div>
+                <button onClick={() => increment()} className='btn btn-primary'>+ num</button>
+                <button onClick={decrement} className='btn btn-primary'>- num</button>
+
+                <h1>{count}</h1>
             </div>
         </>
     )
