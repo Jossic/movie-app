153fb7c3234a265d5cf4d0865c34051fb9844ac7
Jossic <jossic.lapierre@gmail.com>
2020-11-05 13:22:10 +0100

    movie-app - 6 - Class components

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,22 @@
+import React from 'react';
 
-
-
+// Functionnal component
 const About = () => {
     const message = "Hello from Next JS"
     return (
         <h1>{message}</h1>
     )
 }
+
+// Class component
+// class About extends React.Component {
+
+//     render() {
+//         return (
+//             <h1>Composant classe</h1>
+//         )
+//     }
+// }
+
 
 export default About
