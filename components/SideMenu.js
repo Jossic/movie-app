9e72a0c903e2dfccb4a86740ec556827f0929960
Jossic <jossic.lapierre@gmail.com>
2020-11-05 13:43:43 +0100

    movie-app - 7 Reusable Components

---
+++ components/SideMenu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SideMenu = () => {
+    return (
+        <>
+            <h1 className="my-4">Baby-Hoshi</h1>
+            <div className="list-group">
+                <a href="#" className="list-group-item">Category 1</a>
+                <a href="#" className="list-group-item">Category 2</a>
+                <a href="#" className="list-group-item">Category 3</a>
+            </div>
+        </>
+    )
+}
+
+export default SideMenu
