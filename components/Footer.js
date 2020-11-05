a779df9bdbcd6d7a48a38e65ecf51becc7c741bf
Jossic <jossic.lapierre@gmail.com>
2020-11-05 20:47:57 +0100

    movie-app - 24 - App container

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="py-5 bg-dark">
+        <footer className="py-5 bg-dark fixed-bottom">
             <div className="container">
                 <p className="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
             </div>
