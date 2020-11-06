59a3a265bc53078999d3e8d6e6b3e67f74d9df74
Jossic <jossic.lapierre@gmail.com>
2020-11-06 16:17:13 +0100

    movie-app - 36 - Handle genre changes

--- components/MovieCreateForm.js
+++ components/MovieCreateForm.js
@@ -18,6 +18,22 @@ const MovieCreateForm = () => {
         })
     }
 
+    const handleGenreChange = (e) => {
+        const { options } = e.target
+        const optionsLength = options.length
+        let value = []
+
+        for (let i = 0; i < optionsLength; i++) {
+            if (options[i].selected) {
+                value.push(options[i].value)
+            }
+        }
+        setForm({
+            ...form,
+            genre: value.toString()
+        })
+    }
+
     return (
         <form>
             {JSON.stringify(form)}
@@ -92,7 +108,11 @@ const MovieCreateForm = () => {
             </div>
             <div className="form-group">
                 <label htmlFor="genre">Genre</label>
-                <select multiple className="form-control" id="genre">
+                <select
+                    onChange={handleGenreChange}
+                    multiple
+                    className="form-control"
+                    id="genre">
                     <option>drama</option>
                     <option>music</option>
                     <option>adventure</option>
