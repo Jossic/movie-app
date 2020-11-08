5469df3ed41ac81cb2139f2b041b6576ff5ce23f
Jossic <jossic.lapierre@gmail.com>
2020-11-08 21:52:38 +0100

    movie-app - 54 - Post endpoint

--- pages/api/v1/posts.js
+++ pages/api/v1/posts.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
 export default async (req, res) => {
-  const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-  const posts = response.data
-  return res.json(posts.slice(0, 20))
+
+  if (req.method === 'POST') {
+    const postData = JSON.parse(req.body)
+    console.log(postData);
+
+    return res.json({
+      status: 'Post sauvegardé',
+      ...postData
+    })
+  } else {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const posts = response.data
+    return res.json(posts.slice(0, 20))
+  }
 }
--- server/server.js
+++ server/server.js
@@ -84,9 +84,13 @@ app.prepare().then(() => {
         })
     })
 
-    server.get('*', (req, res) => {
-        return handle(req, res)
-    })
+    // server.get('*', (req, res) => {
+    //     return handle(req, res)
+    // })
+
+    // server.post('*', (req, res) => {
+    //     return handle(req, res)
+    // })
 
     const PORT = process.env.PORT || 3000;
 
